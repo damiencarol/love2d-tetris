e43a029e2b07cb73b86aaa87475e4435909c0861
damiencarol <damien.carol@gmail.com>
2024-02-16 14:31:25 +0100

    Fix more color bugs

--- main.lua
+++ main.lua
@@ -11,9 +11,9 @@
 * White:    255:255:255
 ]]--
 color_darkred = {}
-color_darkred.red = 0.72 -- 138
-color_darkred.green = 0.13 -- 76
-color_darkred.blue = 0.11 -- 88
+color_darkred.red = 0.54 -- 138
+color_darkred.green = 0.29 -- 76
+color_darkred.blue = 0.34 -- 88
 
 color_red = {}
 color_red.red = 0.85 -- 217
@@ -21,14 +21,14 @@ color_red.green = 0.38 -- 98
 color_red.blue = 0.45 -- 117
 
 color_roze = {}
-color_roze.red = 230
-color_roze.green = 184
-color_roze.blue = 193
+color_roze.red = 0.9 -- 230
+color_roze.green = 0.72 -- 184
+color_roze.blue = 0.75 -- 193
 
 color_blue = {}
-color_blue.red = 69
-color_blue.green = 107
-color_blue.blue = 115
+color_blue.red = 0.27 -- 69
+color_blue.green = 0.41 -- 107
+color_blue.blue = 0.45 -- 115
 
 
 -- 1 menu
@@ -105,7 +105,7 @@ function love.update(dt)
 end
 
 function piece_blocked()
-    if block.y == 1 then
+    if block.y < 2 then
         return true
     end
     if grid[block.x][block.y-1] == 1 then
@@ -120,6 +120,8 @@ function update_state()
         block.x = 5
         block.y = 20
         sound_block:play()
+        -- score up
+        score = score + 100
     else
         block.y = block.y - 1
         sound_move:play()
@@ -179,6 +181,7 @@ end
 
 function main_title()
     -- background
+    love.graphics.setColor(1,1,1)
     love.graphics.draw(back_image, 0, 0)
 
     -- title
@@ -187,7 +190,6 @@ function main_title()
     love.graphics.print("Tetris", 50+2, 50+2)
     love.graphics.setColor(color_red.red, color_red.green, color_red.blue)
     love.graphics.print("Tetris", 50, 50)
-    love.graphics.setColor(255,255,255)
 
     -- copyright
     love.graphics.setColor(color_darkred.red, color_darkred.green, color_darkred.blue)
@@ -195,17 +197,16 @@ function main_title()
     love.graphics.setColor(color_roze.red, color_roze.green, color_roze.blue)
     love.graphics.rectangle("fill", 150, 480 - 53, 370, 30)
 
-    love.graphics.setColor(color_red.red,0,0)
+    love.graphics.setColor(color_red.red,color_red.green,color_red.blue)
     love.graphics.setFont(font)
     love.graphics.print("Copyright 2024 * Damien CAROL", 160,  480 - 50)
-    love.graphics.setColor(255,255,255)
 end
 
 local backhs_split = 380
 local backhs_thickness = 10
 function draw_high_scores()
     -- background
-    love.graphics.setColor(255,255,255)
+    love.graphics.setColor(1,1,1)
     love.graphics.draw(backhs_image, 0, 0)
 
     -- stats background
@@ -221,15 +222,14 @@ function draw_high_scores()
     love.graphics.print("123456 DAM", backhs_split + 20,  50*1)
     love.graphics.print(" 23456 GREG", backhs_split + 20,  50*2)
     love.graphics.print(" 13456 ALICE", backhs_split + 20,  50*3)
-    love.graphics.setColor(255,255,255)
 end
 
 --local backhs_split = 380
 --local backhs_thickness = 10
 board_width = 300
 board_width_blocks = board_width / 10
-board_height = 400
-board_height_blocks = board_width / 10
+board_height = 480
+board_height_blocks = board_height / 20
 function draw_game()
     -- background
     --love.graphics.draw(backhs_image, 0, 0)
@@ -239,9 +239,9 @@ function draw_game()
     
     -- board background
     love.graphics.setColor(color_darkred.red, color_darkred.green, color_darkred.blue)
-    love.graphics.rectangle("fill", (640-board_width)/2-5, 50-5, board_width+10, board_height+10)
+    love.graphics.rectangle("fill", (640-board_width)/2-5, 0, board_width+10, board_height)
     love.graphics.setColor(0, 0, 0)
-    love.graphics.rectangle("fill", (640-board_width)/2, 50, board_width, board_height)
+    love.graphics.rectangle("fill", (640-board_width)/2, 0, board_width, board_height)
     
     -- set blocks
     love.graphics.setColor(color_roze.red, color_roze.green, color_roze.blue)
@@ -250,18 +250,17 @@ function draw_game()
             if grid[i][j] == 1 then
                 love.graphics.rectangle("fill",
                     (640-board_width)/2 + board_width_blocks*i - board_width_blocks,
-                    50 + board_height - board_height_blocks*j, 
+                    board_height - board_height_blocks*j, 
                     board_width_blocks, board_height_blocks)
             end
         end
     end
 
     -- current block
-    --love.graphics.setColor(color_blue.red, color_blue.green, color_blue.blue)
-    --love.graphics.rectangle("fill",
-    --    (640-board_width)/2 + board_width_blocks*block.x - board_width_blocks,
-    --    50 + board_height - board_height_blocks*block.y, 
-    --    board_width_blocks, board_height_blocks)
+    love.graphics.setColor(color_blue.red, color_blue.green, color_blue.blue)
+    love.graphics.rectangle("fill",
+        (640-board_width)/2 + board_width_blocks*block.x - board_width_blocks,
+        board_height - board_height_blocks*block.y, 
+        board_width_blocks, board_height_blocks)
 
-    love.graphics.setColor(1,1,1)
 end
